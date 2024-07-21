22f4b8f0565fa7788fe2c1037f7e56e71b62eed2
etchedheadplate <1eenqn2j@protonmail.com>
2024-07-21 14:30:39 +0300

    uint256 amount --> uint256 indexed amount

--- src/demos/AutomatedInsurancePayouts/HonestOracle.sol
+++ src/demos/AutomatedInsurancePayouts/HonestOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract HonestOracle {
-    event TransactionEvent(address indexed sender, uint256 amount);
+    event TransactionEvent(address indexed sender, uint256 indexed amount);
 
     function emitEvent(address _sender, uint256 _amount) external {
         emit TransactionEvent(_sender, _amount);
