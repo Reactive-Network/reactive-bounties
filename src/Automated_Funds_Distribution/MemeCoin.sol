305a637b9e63b74f596c8373201a2fda7058627d
PRakhar <srivastavaprakhar3010@gmail.com>
2024-07-24 23:54:11 +0530

    second commit

--- src/Automated_Funds_Distribution/MemeCoin.sol
+++ src/Automated_Funds_Distribution/MemeCoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts/contract/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 
 contract MemeCoin is ERC20 {
