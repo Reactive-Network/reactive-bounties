01eb05b59388e95547548946bdb361cf36e82a32
Harsh kasana <bt22ece106@gmail.com>
2024-07-25 16:36:32 +0530

    samll improvements

--- src/Insurance_payouts/CryptoInsurance.sol
+++ src/Insurance_payouts/CryptoInsurance.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract CryptoInsurance is Ownable{
